599f6948fa246b1d478ac0ed74e81ad51c00f0c3
MYEONGJU\mjk04 <kmj042894@gmail.com>
2025-10-20 22:02:56 +0900

    first commit

---
+++ FunGood_BE/src/main/java/com/fungood/FunGoodBeApplication.java
@@ -0,0 +1,13 @@
+package com.fungood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunGoodBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunGoodBeApplication.class, args);
+	}
+
+}
---
+++ FunGood_BE/src/test/java/com/fungood/FunGoodBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fungood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunGoodBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
