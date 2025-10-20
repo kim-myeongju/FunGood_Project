e1d869eb72332b3624a702ba4a72f5bb79dfa136
MYEONGJU\mjk04 <kmj042894@gmail.com>
2025-10-20 22:36:13 +0900

    마이바티스 연동

---
+++ FunGood_BE/src/main/java/com/fungood/entity/User.java
@@ -0,0 +1,15 @@
+package com.fungood.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String userId;
+    private String userPw;
+
+}
