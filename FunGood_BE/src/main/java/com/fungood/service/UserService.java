b9a2c3071039c74a9f8c581e2b14fc514f0ceee1
MYEONGJU\mjk04 <kmj042894@gmail.com>
2025-11-22 20:53:37 +0900

    #2-FE 회원가입 세팅

--- FunGood_BE/src/main/java/com/fungood/service/UserService.java
+++ FunGood_BE/src/main/java/com/fungood/service/UserService.java
@@ -1,6 +1,6 @@
 package com.fungood.service;
 
-import com.fungood.dto.LoginRequest;
+import com.fungood.dto.user.LoginRequest;
 import com.fungood.entity.User;
 import com.fungood.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
